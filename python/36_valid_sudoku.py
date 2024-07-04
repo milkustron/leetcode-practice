e4060fa918502a5801114c1ed2a4e88049629a6e
milkustron <116026576+milkustron@users.noreply.github.com>
2024-07-04 20:24:57 +0200

    create: 36

---
+++ python/36_valid_sudoku.py
@@ -0,0 +1,30 @@
+class Solution:
+    def isValidSudoku(board) -> bool:    
+        for i in board:
+            memory_row = []
+            for j in range(0,9):
+                if i[j] != ".":
+                    if i[j] not in memory_row:
+                        memory_row.append(i[j])
+                    else: return False
+        
+            
+        for k in range(0,9):
+            memory_column = []
+            for l in board:
+                if l[k] != ".":
+                    if l[k] not in memory_column:
+                        memory_column.append(l[k])
+                    else: return False
+        
+        for z in (0,3,6):
+            for zi in (0,3,6):
+                memory_box = []
+                for w in range(z, z+3):
+                    for y in range(zi, zi+3):  
+                        if board[y][w] != ".":
+                            if board[y][w] not in memory_box:
+                                memory_box.append(board[y][w])
+                            else: return False
+
+        return True
